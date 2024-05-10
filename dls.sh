13bd9cdec4d19786cb4a8a06747e8e1442107b1c
Mike Panciera <5606168+averagehat@users.noreply.github.com>
2024-05-10 17:29:24 -0400

    Update dls.sh

--- dls.sh
+++ dls.sh
@@ -5,5 +5,6 @@ mkdir databases && cd databases &&  \
 tar -xvf k2_viral_20230605.tar.gz -C "viral"  && \
 tar -xvf k2_standard_08gb_20230605.tar.gz -C standard/ && \
 tar -xvf flukraken2.tar.gz -C flu  && \
+wget  https://ftp.ncbi.nlm.nih.gov/genomes/refseq/assembly_summary_refseq.txt
+
 # tar -xvf flu_BC01.fastq.gz  && \
-# wget  https://ftp.ncbi.nlm.nih.gov/genomes/refseq/assembly_summary_refseq.txt
